1193cf09b619c7a23177b654643e8d216aa6be2c
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-04-25 11:14:16 +0530

    add alert

---
+++ src/Alert.js
@@ -0,0 +1,9 @@
+export function Alert(props){
+    return(
+        props.alert && <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            {props.alert.type}-{props.alert.msg}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    );
+}
+
--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import { Alert } from "./Alert";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  //alert hook
+  const[alert,setAlert]=useState(null);
+  function showAlert(message,type){
+      setAlert({
+        msg:message,
+        type: type
+    })
+    setTimeout(() => {  //after 1500ms alert disapper
+      setAlert(null);
+    }, 1500);
+  }
+
+
+  
+//toggle button
   const [myStyle, setMyStyle] = useState({
     color: "black",
     backgroundColor: "white",
@@ -15,6 +33,7 @@ function App() {
         backgroundColor: "black",
       })
       setBtnText("Enable Light Mode")
+      showAlert("Dark mode has been enables","success");
     }
     else
     {
@@ -22,16 +41,27 @@ function App() {
         color: "black",
         backgroundColor: "white",
       })
-      setBtnText("Enable Dark Mode")
+      setBtnText("Enable Dark Mode");
+      showAlert("Light mode has been enables","success");
     }
   }
 
-  return <div className="App">
+
+  
+  return (<div className="App container">
+
+      
+    <Alert alert={alert}/>
+
     <div style={myStyle}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique officiis molestias nemo fugiat consequuntur nostrum soluta voluptatem nisi fugit laborum, exercitationem natus ducimus, voluptate laboriosam officia quibusdam maxime ullam quos temporibus consectetur et harum! Enim repellendus commodi mollitia laborum ex quam? Molestias id voluptatem neque iure consequatur, eum maxime! Assumenda ipsam quasi sequi ipsum totam voluptatum sit quaerat veritatis alias deleniti eveniet, obcaecati accusamus laboriosam ad officiis nesciunt reiciendis explicabo recusandae praesentium sapiente. Ipsam, ipsum quas magnam est repellat dicta animi ex, quasi nesciunt nam quos id, ratione sequi voluptatum dolorum veniam quam. Tempore dolorum distinctio et facilis fuga.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique 
+          officiis molestias nemo fugiat consequuntur nostrum soluta voluptatem nisi 
+          fugit laborum, exercitationem natus ducimus, voluptate laboriosam officia 
+          quibusdam maxime ullam quos temporibus consectetur et harum! Enim repellendus 
+          commodi mollitia laborum </p>
     </div>
     <button onClick={toggleStyle}>{btnText}</button>
-  </div>;
+  </div>);
 }
 
 export default App;
