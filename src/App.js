d11ac89d2b3dc062b268afba1110577aa8c8fc4d
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-04-25 10:43:43 +0530

    add toggle button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+  const [btnText, setBtnText] = useState("Enable Dark Mode");
+
+  function toggleStyle(){
+    if(myStyle.color==='black'){
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+      })
+      setBtnText("Enable Light Mode")
+    }
+    else
+    {
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      })
+      setBtnText("Enable Dark Mode")
+    }
+  }
+
+  return <div className="App">
+    <div style={myStyle}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique officiis molestias nemo fugiat consequuntur nostrum soluta voluptatem nisi fugit laborum, exercitationem natus ducimus, voluptate laboriosam officia quibusdam maxime ullam quos temporibus consectetur et harum! Enim repellendus commodi mollitia laborum ex quam? Molestias id voluptatem neque iure consequatur, eum maxime! Assumenda ipsam quasi sequi ipsum totam voluptatum sit quaerat veritatis alias deleniti eveniet, obcaecati accusamus laboriosam ad officiis nesciunt reiciendis explicabo recusandae praesentium sapiente. Ipsam, ipsum quas magnam est repellat dicta animi ex, quasi nesciunt nam quos id, ratione sequi voluptatum dolorum veniam quam. Tempore dolorum distinctio et facilis fuga.</p>
     </div>
-  );
+    <button onClick={toggleStyle}>{btnText}</button>
+  </div>;
 }
 
 export default App;
